883796e9f40dab321a04414c8a4e25ef78efd400
ulesta <justincuaresma@gmail.com>
2014-07-19 13:58:13 -0400

    Ch16 completed
    Added:
    + alternate menu item: "Show Subtitle"
    + completed challenge: added empty view for ListFragment prompting the
    users that they have to create a new crime when list is empty

--- src/com/atleusdigital/android/criminalintent/CrimeListFragment.java
+++ src/com/atleusdigital/android/criminalintent/CrimeListFragment.java
@@ -2,17 +2,22 @@ package com.atleusdigital.android.criminalintent;
 
 import java.util.ArrayList;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -24,6 +29,9 @@ public class CrimeListFragment extends ListFragment {
 	public static final String KEY_UUID = "crime_key";
 	
 	private ArrayList<Crime> mCrimes;
+	private boolean mSubtitleVisible;
+	
+	private Button mEmptyButton;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -33,9 +41,50 @@ public class CrimeListFragment extends ListFragment {
 		getActivity().setTitle(R.string.crimes_title);
 		mCrimes = CrimeLab.get(getActivity()).getCrimes();
 		
+		// to retain subtitle's visibility
+		setRetainInstance(true);
+		mSubtitleVisible = false;
+		
 		CrimeAdapter adapter = new CrimeAdapter(mCrimes);
 		setListAdapter(adapter);
 	}
+	
+	
+	@TargetApi(11)
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		/* remember that view gets recreated after a config change,
+		 * in this case we make sure we set the subtitle to show if it was already showing
+		 */
+		//View v = super.onCreateView(inflater, container, savedInstanceState);
+		
+		if ( Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB ) {
+			if (mSubtitleVisible) {
+				getActivity().getActionBar().setSubtitle(R.string.subtitle);
+			}
+		}
+		
+		View v = inflater.inflate(R.layout.fragment_crimelist, null);
+		
+		// wire up functionality to empty button
+		mEmptyButton = (Button) v.findViewById(R.id.fragment_crimelist_empty_button);
+		mEmptyButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Crime crime = new Crime();
+				CrimeLab.get(getActivity()).addCrime(crime);
+				Intent i = new Intent(getActivity(), CrimePagerActivity.class);
+				i.putExtra(CrimeFragment.EXTRA_CRIME_ID, crime.getmId());
+				startActivityForResult(i, 0);
+			}
+		});
+		
+		return v;
+	}
+
+
 
 	@Override
 	public void onListItemClick(ListView l, View v, int position, long id) {
@@ -82,9 +131,13 @@ public class CrimeListFragment extends ListFragment {
 		super.onCreateOptionsMenu(menu, inflater);
 		// must be called to inflate our menu in res/menu
 		inflater.inflate(R.menu.fragment_crime_list, menu);
+		MenuItem showSubtitle = menu.findItem(R.id.menu_item_show_subtitle);
+		if (mSubtitleVisible && showSubtitle != null) {
+			showSubtitle.setTitle(R.string.hide_subtitle);
+		}
 	}
 	
-
+	@TargetApi(11)
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
@@ -94,6 +147,19 @@ public class CrimeListFragment extends ListFragment {
 			Intent i = new Intent(getActivity(), CrimePagerActivity.class);
 			i.putExtra(CrimeFragment.EXTRA_CRIME_ID, crime.getmId());
 			startActivityForResult(i, 0);
+			return true;
+		case R.id.menu_item_show_subtitle:
+			if (getActivity().getActionBar().getSubtitle() == null) {
+				getActivity().getActionBar().setSubtitle(R.string.subtitle);
+				// to retain subtitle's visibility
+				mSubtitleVisible = true;
+				item.setTitle(R.string.hide_subtitle);
+			}
+			else {
+				getActivity().getActionBar().setSubtitle(null);
+				mSubtitleVisible = false;
+				item.setTitle(R.string.show_subtitle);
+			}
 			return true;
 		default:
 			return super.onOptionsItemSelected(item);
