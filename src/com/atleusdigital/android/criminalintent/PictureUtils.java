db28330e3055b16b38585b22a479f4c325e019e8
Justin Cuaresma <justincuaresma@gmail.com>
2014-09-07 22:15:22 -0400

    [Ch20 - update] Created PictureUtils class
    Added:
    + PictureUtils class which returns a scaled Drawable
    
    To-do:
    + Wire up ImageView to display thumbnail

---
+++ src/com/atleusdigital/android/criminalintent/PictureUtils.java
@@ -0,0 +1,46 @@
+package com.atleusdigital.android.criminalintent;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.view.Display;
+
+public class PictureUtils {
+	/**
+	 * Get a BitmapDrawawble from a local file that is scaled down
+	 * to fit the current Window size.
+	 */
+	
+	@SuppressWarnings("deprecation")
+	public static BitmapDrawable getScaledDrawable(Activity a, String path) {
+		Display display = a.getWindowManager().getDefaultDisplay();
+		float destWidth = display.getWidth();
+		float destHeight = display.getHeight();
+		
+		// Read in the dimensions of the image on disk
+		BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inJustDecodeBounds = true;
+		BitmapFactory.decodeFile(path, options);
+		
+		float srcWidth = options.outWidth;
+		float srcHeight = options.outHeight;
+		
+		int inSampleSize = 1;
+		// if image is too big for the activity window, get max(height, width)
+		if ( srcHeight > destHeight || srcWidth > destWidth) {
+			if (srcWidth > srcHeight) {
+				inSampleSize = Math.round(srcHeight / destHeight);
+			} else {
+				inSampleSize = Math.round(srcWidth / destWidth);
+			}
+		}
+		
+		options = new BitmapFactory.Options();
+		options.inSampleSize = inSampleSize;
+		
+		Bitmap bitmap = BitmapFactory.decodeFile(path, options);
+		return new BitmapDrawable(a.getResources(), bitmap);
+	}
+	
+}
