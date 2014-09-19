8465fe689a08453deafb5f2947007f72487ba429
Justin Cuaresma <justincuaresma@gmail.com>
2014-09-18 21:32:07 -0400

    [Ch21 - update] Wired Send Button
    Added:
    + wired up Send button to fire implicit intent
    
    Notes:
    + <intent-filter> is used in apps that would be used given a specific
    implicit intent; i.e.:<action android:name="android.intent.action.VIEW"
    />
    + in addition, this line must be explicitly stated: <category
    android:name="android.intent.category.DEFAULT" />

--- src/com/atleusdigital/android/criminalintent/CrimeFragment.java
+++ src/com/atleusdigital/android/criminalintent/CrimeFragment.java
@@ -286,6 +286,32 @@ public class CrimeFragment extends Fragment {
 			}
 		});
 		
+		Button chooseButton = (Button)v.findViewById(R.id.crime_choose);
+		chooseButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				
+			}
+		});
+		
+		Button reportButton = (Button)v.findViewById(R.id.crime_send);
+		reportButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// cool
+				//Intent i = new Intent(Intent.ACTION_VOICE_COMMAND);
+				Intent i = new Intent(Intent.ACTION_SEND);
+				i = Intent.createChooser(i, getString(R.string.send_report));
+				i.setType("text/plain");
+				i.putExtra(Intent.EXTRA_TEXT, getCrimeReport());
+				i.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.crime_report_subject));
+				startActivity(i);
+			}
+		});
+		
 		return v;
 	}
 	
